6e76c10b1cdaa48acf2b8baedbb0ffbb0b71d08f
Arthur Rohr Paschoal Correa Cardoso <57691542+ArthurRohr@users.noreply.github.com>
2023-02-03 17:15:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function modal() {
+    let modal = document.querySelector('.modal-atend')
+
+    modal.style.display = 'block';
+}
+
+function fechar() {
+    let modal = document.querySelector('.modal-atend')
+
+    modal.style.display = 'none';
+}
+
+function button1() {
+    let modal = document.querySelector('#b1')
+
+    modal.style.display = 'block';
+}
+
+function sumir1() {
+    let modal = document.querySelector('#b1')
+
+    modal.style.display = 'none';
+}
+
+function button2() {
+    let modal = document.querySelector('#b2')
+
+    modal.style.display = 'block';
+}
+
+function sumir2() {
+    let modal = document.querySelector('#b2')
+
+    modal.style.display = 'none';
+}
+
+function button3() {
+    let modal = document.querySelector('#b3')
+
+    modal.style.display = 'block';
+}
+
+function sumir3() {
+    let modal = document.querySelector('#b3')
+
+    modal.style.display = 'none';
+}
+
+function button4() {
+    let modal = document.querySelector('#b4')
+
+    modal.style.display = 'block';
+}
+
+function sumir4() {
+    let modal = document.querySelector('#b4')
+
+    modal.style.display = 'none';
+}
